69478224e97c76ac7b1934d726f4094291367265
Sawyer X <xsawyerx@cpan.org>
2010-05-24 10:55:27 +0300

    pushing all changes that i somehow left out, releasing 0.06

--- lib/Module/Version.pm
+++ lib/Module/Version.pm
@@ -7,7 +7,7 @@ use base 'Exporter';
 use Carp;
 use ExtUtils::MakeMaker;
 
-our $VERSION   = '0.05';
+our $VERSION   = '0.06';
 our @EXPORT_OK = 'get_version';
 
 sub get_version {
@@ -29,7 +29,7 @@ Module::Version - Get module versions
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =head1 SYNOPSIS
 

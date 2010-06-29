a7fe7f1cc51ee235ea0b5970275df25b8273e33e
Sawyer X <xsawyerx@cpan.org>
2010-06-29 23:41:44 +0300

    releasing 0.12

--- lib/Module/Version.pm
+++ lib/Module/Version.pm
@@ -7,7 +7,7 @@ use base 'Exporter';
 use Carp;
 use ExtUtils::MakeMaker;
 
-our $VERSION   = '0.11';
+our $VERSION   = '0.12';
 our @EXPORT_OK = 'get_version';
 
 sub get_version {
@@ -29,7 +29,7 @@ Module::Version - Get module versions
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =head1 SYNOPSIS
 

d1d88cfd2c889236b6971c813bbed2bb911a7600
Sawyer X <xsawyerx@cpan.org>
2010-06-29 08:52:21 +0300

    doc fixes for 0.11

--- lib/Module/Version.pm
+++ lib/Module/Version.pm
@@ -7,7 +7,7 @@ use base 'Exporter';
 use Carp;
 use ExtUtils::MakeMaker;
 
-our $VERSION   = '0.10';
+our $VERSION   = '0.11';
 our @EXPORT_OK = 'get_version';
 
 sub get_version {
@@ -29,34 +29,34 @@ Module::Version - Get module versions
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =head1 SYNOPSIS
 
 This module fetches the version of any other module.
 
-It comes with a CLI program C<module-version> which does the same.
+It comes with a CLI program C<mversion> which does the same.
 
     use Module::Version 'get_version';
 
     print get_version('Search::GIN'), "\n";
 
-Or using C<module-version>:
+Or using C<mversion>:
 
-    $ module-version Search::GIN
+    $ mversion Search::GIN
     0.04
 
-    $ module-version Doesnt::Exist
+    $ mversion Doesnt::Exist
     Warning: module 'Doesnt::Exist' does not seem to be installed.
 
-    $ module-version --quiet Doesnt::Exist
+    $ mversion --quiet Doesnt::Exist
     (no output)
 
-    $ module-version --full Search::GIN Moose
+    $ mversion --full Search::GIN Moose
     Search::GIN 0.04
     Moose 1.01
 
-    $ module-version --input modules.txt
+    $ mversion --input modules.txt
     Search::GIN 0.04
     Data::Collector 0.03
     Moose 1.01
@@ -85,41 +85,14 @@ Sawyer X, C<< <xsawyerx at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to
-C<bug-module-version at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Module-Version>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as I
-make changes.
+Please report bugs and other issues on the bugtracker:
+
+L<http://github.com/xsawyerx/module-version/issues>
 
 =head1 SUPPORT
 
-This module sports 100% test coverage, but in case you have more issues...
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Module::Version
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Module-Version>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Module-Version>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Module-Version>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Module-Version/>
-
-=back
+This module sports 100% test coverage, but in case you have more issues, please
+see I<BUGS> above.
 
 =head1 LICENSE AND COPYRIGHT
 
